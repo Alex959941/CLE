4d1a6427934ea2e24bbba693a4995b55accbd1db
Alex959941 <1469436@sd61learn.ca>
2026-06-09 21:00:48 -0700

    Add service worker registration to quiz script

--- myscript.js
+++ myscript.js
@@ -1,3 +1,15 @@
+// load the service worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('sw.js').then(function(registration) {
+      console.log('Service Worker registered with scope:', registration.scope);
+    }, function(error) {
+      console.log('Service Worker registration failed:', error);
+    });
+  });
+}           
+
+
 let quizData = [
     {
        question: "The penalty for an individual knowingly transmitting a false distress is:",
